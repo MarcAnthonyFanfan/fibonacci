c1f7b2bea23f54837874cfb415ba7b2f194b1270
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-10-18 13:57:10 -0400

    Added test script

---
+++ test/run_all_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in {-1..47}; do ../bin/fib $i > ../temp/t$i.txt; done
+for i in {-1..47}; do
+	TEST_DIFF=$(diff ./t${i}.txt ../temp/t${i}.txt)
+	if [[ -n $TEST_DIFF ]]; then
+		printf "FAILED TEST Fib(${i})\nOUTPUT:\n${TEST_DIFF}\n"
+	else
+		printf "PASSED TEST Fib(${i})\n"
+	fi
+done
